742fca6d15e1e6eb36e9bf30870aedc1d1831cee
Stephen Sarquah <stephen.sarquah@gmail.com>
2019-09-09 12:39:32 +0200

    Add project files.

---
+++ DB-ApplicantChallenge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DB_ApplicantChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
